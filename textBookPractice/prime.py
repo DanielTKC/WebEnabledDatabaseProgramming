cc48becbefa97337b01f0a97209d98262b010b02
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-09-07 09:24:10 -0500

    added the oddCheck loop and completed program

--- textBookPractice/prime.py
+++ textBookPractice/prime.py
@@ -7,5 +7,16 @@ print("1 is not a prime number")
 for iPrime in range(2, 101) :
     if iPrime == 2 :
         print (iPrime, "is a prime number")
-    elif iPrime % 2 == 0 :
+    elif iPrime % 2 == 0 : #If it is divisible by 2 and is not 2, then it is not prime.
         print(iPrime, "is NOT a prime number")
+    else :
+# I've already checked the numbers divisible by 2 so now i need to check odd numbers
+        bIsPrime = True #need this to hold the value of true or false so I can use a conditional to print
+        for oddCheck in range(3, iPrime, 2): #starts at 3, goes to iPrime -1, in increments of 2(odd)
+            if iPrime % oddCheck == 0:
+                bIsPrime = False
+                break #break out of the loop if it is false
+        if bIsPrime :
+            print(iPrime, "is a prime number")
+        else :
+            print(iPrime, "is not a prime number")
