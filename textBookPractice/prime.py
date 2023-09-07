75c498e22a0fd4657e61fd724e5fce977e847da5
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-09-07 09:18:14 -0500

    Covered number 1 and even numbers

--- textBookPractice/prime.py
+++ textBookPractice/prime.py
@@ -2,3 +2,10 @@
 
 #Description: A program that determines if a number between 1 & 100 is prime.
 
+#Let's deal with 1
+print("1 is not a prime number")
+for iPrime in range(2, 101) :
+    if iPrime == 2 :
+        print (iPrime, "is a prime number")
+    elif iPrime % 2 == 0 :
+        print(iPrime, "is NOT a prime number")
