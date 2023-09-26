085f07bf3101d2e20d557fc7fcfb9b10c642bb7f
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-09-26 18:02:39 -0500

    Finshed sorting and printing tuples

--- labs/dataStructuresBasicsLab.py
+++ labs/dataStructuresBasicsLab.py
@@ -31,11 +31,30 @@ for iCount in range(100) :
  #   strQueue.pop(0)
 
 # making a copy of the queue as a list.
-for customer in list(strQueue) :
-    if customer not in dictCustomer :
-        dictCustomer[customer] = 0
-    dictCustomer[customer]
-    dictCustomer[customer] += randomBurgers()
+for sCustomer in list(strQueue) :
+    if sCustomer not in dictCustomer :
+        dictCustomer[sCustomer] = 0
+    dictCustomer[sCustomer]
+    dictCustomer[sCustomer] += randomBurgers()
     strQueue.pop(0)
+
+# Making sure the dictionary is functioning properly.
+# print(dictCustomer)
+listSortedCustomers = []
+
+for i in dictCustomer :
+    k = (i, dictCustomer[i])
+    listSortedCustomers.append(k)
+
+#sorts the list by amount of burgers eaten.
+listSortedCustomers.sort(key = lambda x: x[1], reverse = True)
+
+for tplCustomer in listSortedCustomers :
+    name, burgers = tplCustomer
+    print(f'{name:20s}\t\t{burgers:3d}')
+
+
+
+
 
 
