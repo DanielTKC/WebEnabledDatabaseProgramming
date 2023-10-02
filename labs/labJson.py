ef6c54d1295f128f316664e0ef5c005c5425f84c
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-10-02 17:11:31 -0500

    Finished traversing dict and calculated average age and duration.

--- labs/labJson.py
+++ labs/labJson.py
@@ -8,15 +8,40 @@ with open('astronauts.json') as fInput :
     astros = json.load(fInput)
 
 count = 0
-sum_total_duration = 0
-sum_age_at_mission = 0
+dSumTotalDuration = 0
+iSumAgeAtMission = 0
 
 
 for astronaut in astros :
    count +=1
 
    sName = astronaut['Profile']['Name']
-   print(sName)
+   iMissionYear = astronaut['Mission']['Year']
+   iBirthYear  = astronaut['Profile']['Birth Year']
+   dMissionDuration = astronaut['Mission']['Durations']['Mission duration']
+   dSumTotalDuration += dMissionDuration
+   iAgeAtMission = iMissionYear - iBirthYear
+   iSumAgeAtMission += iAgeAtMission
+   
+
+
+
+   print(f"Record number: {count}")
+   print(f"Name: {sName}")
+   print(f"Mission Year: {iMissionYear}")
+   print(f"Birth Year: {iBirthYear}\n")
+
+#  print(f"Age at mission: {iAgeAtMission}")
+#  print(f"Duration: {dMissionDuration}")
+
+dAverageMissionDuration = dSumTotalDuration / count
+dAverageAgeAtMission = round(iSumAgeAtMission / count)
+print(f"The average age of an astronaut at the time of their mission is {dAverageAgeAtMission} years old")
+print(f"The average mission duration is: {dAverageMissionDuration:.2f}")
+
+
+
+
 
 
 
