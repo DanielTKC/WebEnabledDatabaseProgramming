c3ce5e191164f3d6b9c36c879257a4b1af53c6d4
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-11-07 21:02:14 -0600

    fixed charfield error on password

--- MyTravel/travelsites/models.py
+++ MyTravel/travelsites/models.py
@@ -24,7 +24,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     user_name = models.CharField(max_length=20)
-    password = models.EmailField(max_length=20)
+    password = models.CharField(max_length=20)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=13, blank=True)
     destinations = models.ManyToManyField(Destination, blank=True)
