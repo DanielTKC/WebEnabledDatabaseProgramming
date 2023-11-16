7cb5bba79dbfc3426610a34cb9e2aef9e8b6f2fd
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-11-16 13:29:12 -0600

    employee page, skill level, contact info

--- MyTravel/homepages/admin.py
+++ MyTravel/homepages/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Contact_Information, Employee, State, Skill
+from .models import Contact_Information, Employee, State, Skill, Skill_Level
 
 admin.site.register(State)
 admin.site.register(Skill)
 admin.site.register(Contact_Information)
 admin.site.register(Employee)  
+admin.site.register(Skill_Level)
 
 # Register your models here.
--- MyTravel/homepages/models.py
+++ MyTravel/homepages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Contact_Information(models.Model):
             contact_phone = models.CharField(max_length=10)
@@ -16,8 +17,8 @@ class Skill(models.Model) :
             description = models.CharField(max_length=30)
         
             def __str__(self):
-                return (self.description)   
-
+                return (self.description)
+        
 class Employee(models.Model):
             TITLE = (
                 ('Ms.', 'MISS'),
@@ -26,13 +27,24 @@ class Employee(models.Model):
                 ('Miss', 'Miss'),
                 ('Mx.', 'MX.'),
             )
+        
             emp_first = models.CharField(max_length=20)
             emp_last = models.CharField(max_length=20)
             emp_title = models.CharField(max_length=4, choices=TITLE, null=True, blank=True)
             emp_state = models.ForeignKey('State', null=True, blank=True, on_delete=models.SET_NULL)
             contact_information = models.OneToOneField(Contact_Information, on_delete=models.CASCADE)
-            emp_skills = models.ManyToManyField('Skill')
             
-            def __str__(self): 
-                    return (self.emp_first + " " + self.emp_last)    
-
+            #Add this line
+            emp_skills = models.ManyToManyField('Skill', through='Skill_Level')
+        
+            def __str__(self):
+                return (self.emp_first + " " + self.emp_last)
+        
+        #Add this class
+class Skill_Level(models.Model):
+            employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+            skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+            skill_rating = models.PositiveIntegerField(default=3, validators=[MinValueValidator(0), MaxValueValidator(5)])
+        
+            def __str__(self):
+                return '%s %s %s %i' %(self.employee.emp_first,self.employee.emp_last,self.skill.description,self.skill_rating)
