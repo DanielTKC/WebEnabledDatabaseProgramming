4b0f640a8bcc399e2549a3e8192cb59256ae6e72
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-11-16 13:32:34 -0600

    search and find emp paths to urls.py

--- MyTravel/homepages/urls.py
+++ MyTravel/homepages/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from .views import indexPageView
 from .views import aboutPageView
 from .views import empPageView
+from .views import searchEmpPageView
+from .views import findEmpPageView
 
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("about/", aboutPageView, name="about"),
     path("emp/", empPageView, name="employee"),
+    path("searchemp/", searchEmpPageView, name="searchemp"),
+    path("findemp/", findEmpPageView, name="findemp"), 
 ]
 
