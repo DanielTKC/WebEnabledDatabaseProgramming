5ca7f062cc5533e6032ae35ef1f1a412d7649c17
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2023-11-15 19:55:48 -0600

    added contact information model to homepages.

--- MyTravel/homepages/models.py
+++ MyTravel/homepages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Contact_Information(models.Model):
+            contact_phone = models.CharField(max_length=10)
+        
+            def __str__(self):
+                phone = '(%s) %s-%s' %(self.contact_phone[0:3],self.contact_phone[3:6],self.contact_phone[6:10])
+                return phone        
